e660e67fae11c186522dff8a6643459ef70a6e45
Alexander Bryakin <alexandebryakin@gmail.com>
2022-09-12 19:07:06 +0300

    [feat] add GetPassport query + tests

--- spec/factories/passport_factory.rb
+++ spec/factories/passport_factory.rb
@@ -7,6 +7,16 @@ FactoryBot.define do
     middle_name  { 'Metson' }
     sequence(:code) { (9_999 - _1).to_s }
     sequence(:number) { (999_999 - _1).to_s }
-    verified { false }
+
+    trait :with_image do
+      after :create do |passport|
+        attachment = ActiveStorage::Blob.create_and_upload!(
+          io: File.open('spec/fixtures/files/passport.jpeg'),
+          filename: 'passport.jpeg',
+          content_type: 'image/jpeg'
+        )
+        passport.image.attach(attachment)
+      end
+    end
   end
 end
